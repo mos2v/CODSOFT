c04984a7b9e63c95430c4ee3f95d99d5957a0c6c
mos2v <officialmostafa@protonmail.com>
2024-07-07 17:06:06 +0300

    first commit

---
+++ TicTacToe/src.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+import pygame
+import sys
+
+pygame.init()
+
+White = (255, 255, 255)
+Gray = (180, 180, 180)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+
+
+def minimax(board):
+    {
+
+    }    
+        
+    
